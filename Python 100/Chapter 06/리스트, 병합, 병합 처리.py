8df303bbb00fa2e4d0397af5d6d122f3985fef14
Inhyuk Woo / 우인혁 <96756113+Y-mannn@users.noreply.github.com>
2022-06-14 16:48:34 +0900

    Add files via upload

---
+++ Python 100/Chapter 06/리스트, 병합, 병합 처리.py
@@ -0,0 +1,26 @@
+#2개의 리스트를 하나의 리스트로 묶는 병합 처리에 대한 코드를 구현하시오.
+#이 문제는 리스트와 리스트를 하나로 병합하는 방법에 대해서 아는지를 묻는 문제이다.
+
+#[1]: a, b 2개의 리스트
+a = [0, 1, 2, 3, 4, 4]
+b = [5, 6, 7, 8, 9, 4]
+
+#[2]: a, b 리스트 --> 하나로 병합 --> 더하기(+) 연산자 사용.
+ab_plus = a + b
+print('[1-1]a + b 병합 리스트', ab_plus, len(ab_plus), '개 요소')
+
+#[3]: a * b 병합 --> 지원 x --> 리스트 * 숫자하고의 곱셈은 가능
+ab_multiplication = a * 3
+print('[1-2]a * 3 병합 리스트', ab_multiplication, len(ab_multiplication), '개 요소')
+
+#[4]: c, d 2개의 리스트
+c = (0, 1, 2, 3, 4, 4)
+d = (5, 6, 7, 8, 9, 4)
+
+#[5]: c, d 리스트 --> 하나로 병합 --> 더하기(+) 연산자 사용.
+ab_plus = c + d
+print('[2-1]a + b 병합 리스트', ab_plus, len(ab_plus), '개 요소')
+
+#[6]: c * d 병합 --> 지원 x --> 리스트 * 숫자하고의 곱셈은 가능
+ab_multiplication = a * 3
+print('[2-2]c * 3 병합 리스트', ab_multiplication, len(ab_multiplication), '개 요소')
---
+++ Python 100/Chapter 06/리스트, 오름차순, 점수.py
@@ -0,0 +1,21 @@
+#학생들의 영어 점수를 오름차순으로 출력하는 코드를 구현하시오.
+#영어 점수는 리스트로 구성한다.
+#내림차순도 구현해본다.
+
+#[1]: 영어 점수
+eng_scores = [100, 30, 80, 90, 50]
+
+#[2]: 정렬 --> sort() 또는 sorted()함수 사용 --> 정렬 디폴트가 오름차순
+eng_scores.sort()
+
+#[3]: 반복문을 사용해서 출력하기
+for i in range(len(eng_scores)):
+    print(eng_scores[i], end='\t')
+print()
+
+#[4]: 내림차순으로 정렬
+eng_scores.sort(reverse=True) #오름차순 --> 내림차순
+
+for i in range(len(eng_scores)):
+    print(eng_scores[i], end='\t')
+print()
