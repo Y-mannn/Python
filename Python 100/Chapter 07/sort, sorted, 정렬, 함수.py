58a073c8d6f964e854d182768efed0fd2229e7f6
Inhyuk Woo / 우인혁 <96756113+Y-mannn@users.noreply.github.com>
2022-06-19 20:39:03 +0900

    Add files via upload

--- Python 100/Chapter 07/sort, sorted, 정렬, 함수.py
+++ Python 100/Chapter 07/sort, sorted, 정렬, 함수.py
@@ -7,12 +7,13 @@ lst = [100, 50, 40, 70, 90, 60, 80]
 #[2]: sort() 함수를 이용한 오름차순, 내림차순 정렬
 #sort()함수나 sorted()함수나 아무런 옵션이 없이 사용하면 디폴트 정렬 --> 오름차순
 lst.sort() #오름차순 sort() = sort(reverse=False)
-print(lst)
+print('[원본-1]: ', lst)
 
 lst.sort(reverse=True) #내림차순 --> 먼저 오름차순으로 정렬이 되고나서 내림차순으로 정렬 --> 원본을 오름차순으로 정렬하고 역순으로 정렬
-print(lst)
+print('[원본-2]: ', lst)
 
 #[3]: sorted()
 #reverse 옵션을 안쓰면 디폴트가 False가 되므로 기본 정렬 --> 오름차순 --> reverse=False
-acs_lst = sorted(lst)
-print(acs_lst)
+acs_lst = sorted(lst) #원본 배열인 lst 리스트는 그대로 유지
+print('[변경]: ', acs_lst)
+print('[원본-2]: ', lst)
